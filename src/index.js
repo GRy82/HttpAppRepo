73bc4332c7d7065a0355e330ebdca2c1b6311d5e
Gregory Ryan <gwryan@buffalo.edu>
2021-08-09 17:03:17 -0400

    Sentry logging extracted into designated logging service.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import * as Sentry from "@sentry/react";
-import { Integrations } from "@sentry/tracing";
+import logger from './services/logService';
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 
-Sentry.init({
-    dsn: "https://dbbb6194e3b6443e9b3320808a8fb47d@o950876.ingest.sentry.io/5899522",
-    integrations: [new Integrations.BrowserTracing()],
-  
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production
-    tracesSampleRate: 1.0,
-  });
+logger.init();
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
