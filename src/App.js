913b8621909e98e747b2fe1c1862707b493ee9ad
Gregory Ryan <gwryan@buffalo.edu>
2021-08-09 16:21:08 -0400

    axios interceptor extracted into separate module, httpService.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
-import axios from 'axios';
+import http from './services/httpService';
 
 const apiEndpoint = 'http://jsonplaceholder.typicode.com/posts';
-
-//parameters are a success callback, and an error callback.
-axios.interceptors.response.use(null, error => {
-  const expectedError = error.response && error.response.status >= 400 && error.response.status < 500;
-  if(!expectedError){
-    console.log("logging unexpected error", error);
-    alert("An unexpected error has occurred.");
-  }
-
-  return Promise.reject(error);
-});
 
 class App extends Component {
   state = {
@@ -22,20 +11,20 @@ class App extends Component {
 
 
   async componentDidMount() {
-    const { data: posts } = await axios.get(apiEndpoint);
+    const { data: posts } = await http.get(apiEndpoint);
     this.setState({ posts });
   }
 
   handleAdd = async() => {
     const obj = { title: 'a', body: 'b' };
-    const { data: post } = await axios.post(apiEndpoint, obj);
+    const { data: post } = await http.post(apiEndpoint, obj);
     this.setState({ posts: [post, ...this.state.posts] });
   };
 
   handleUpdate = async post => {
     //put updates all properties, patch updates 1 or more properties.
     post.title = "UPDATED";
-    await axios.put(`${apiEndpoint}/${post.id}`, post);
+    await http.put(`${apiEndpoint}/${post.id}`, post);
     let posts = [...this.state.posts];
     let index = posts.indexOf(post);
     posts[index] = { ...post };
@@ -48,7 +37,7 @@ class App extends Component {
     const posts = this.state.posts.filter(e => e.id !== post.id);
     this.setState({ posts });
     try{
-      await axios.delete(`${apiEndpoint}/${post.id}`);
+      await http.delete(`${apiEndpoint}/${post.id}`);
       //throw new Error("Sorry, something went wrong.");
     }
     catch(ex){
